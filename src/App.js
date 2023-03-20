053d0dc3a4610f8e7077a40e6818c50183c4b588
bakuyy <sophieyang12345@gmail.com>
2023-03-20 19:45:04 -0400

    adding to homepage

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
 import './App.css';
+import React, {useState} from 'react'
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Classes from './pages/Classes'
 import Notes from './pages/Notes'
 import Clubs from './pages/Clubs';
 
-function App() {
+function TodoForm(props) {
+  const [input, setInput] = useState('');
+
+  const handleSubmit = e=> {
+    e.preventDefault();
+    // props.onSubmit({
+    //   id: Math.floor(Math.random()*10000),
+    //   text:input
+    // })
+    setInput('')
+  }
+
+  const handleChange = e => {
+    setInput(e.target.value)
+  }
   return (
     <div className="App">
       <BrowserRouter>
         <Navbar/>
           <Routes>
             <Route path='/' exact element={<Notes/>}></Route>
-            <Route path='/problem' exact element={<Classes/>}></Route>
+            <Route path='/classes' exact element={<Classes/>}></Route>
             <Route path='/main' exact element={<Clubs/>}></Route>
           </Routes>
       </BrowserRouter>
+    <div>
+      <form class='todo-form' onSubmit={handleSubmit}>
+        <input
+        type="text" 
+        placeholder='Add a todo' 
+        value={input}
+        name="text"
+        onChange={handleChange}/>
+      <button>Add Todo</button>
+      </form>
+    </div>
     </div>
   );
 }
 
-export default App;
+export default TodoForm;
--- src/pages/Classes.js
+++ src/pages/Classes.js
@@ -1,11 +1,25 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const Classes = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+  // const [input, setInput] = useState('')
+
+  // const handleSubmit = e=> {
+  //   e.preventDefault();
+  //   props.onSubmit({
+  //     id: Math.floor(Math.random()*10000),
+  //     text:input
+  //   })
+    
+  // }
+  // return (
+  //   <div>
+  //     <form class='todo-form' onSubmit={handleSubmit}>
+  //       <button className="p-4px text-3xl bg-[#014FB3]">Add Todo</button>
+  //       <input type="text" placeholder='Add a todo' value={input}
+  //       name="text"/>
+  //     </form>
+  //   </div>
+  // )
 }
 
 export default Classes
